08a32677f371b5043ea2e8eac6c349b4b243061b
Grigore Stefan <g_stefan@yahoo.com>
2021-11-15 23:06:41 +0200

    Update

--- source/file-to-cs-version.hpp
+++ source/file-to-cs-version.hpp
@@ -10,10 +10,10 @@
 #ifndef FILE_TO_CS_VERSION_HPP
 #define FILE_TO_CS_VERSION_HPP
 
-#define FILE_TO_CS_VERSION_ABCD                1,8,0,22
-#define FILE_TO_CS_VERSION_STR                 "1.8.0"
-#define FILE_TO_CS_VERSION_STR_BUILD           "22"
-#define FILE_TO_CS_VERSION_STR_DATETIME        "2021-08-09 17:38:03"
+#define FILE_TO_CS_VERSION_ABCD                1,9,0,23
+#define FILE_TO_CS_VERSION_STR                 "1.9.0"
+#define FILE_TO_CS_VERSION_STR_BUILD           "23"
+#define FILE_TO_CS_VERSION_STR_DATETIME        "2021-11-15 16:51:47"
 
 #ifndef XYO_RC
 
